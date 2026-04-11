75dcc5fbb41ce136204ca8c41f765c47a0483601
hellomrli <63144574+hellomrli@users.noreply.github.com>
2026-04-11 15:14:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,3 @@
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# 打开你的云编译仓库里的 diy-part2.sh (或者在 .yml 文件的相关运行步骤中)
-# 添加下面这行代码：
-sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.18/g' target/linux/x86/Makefile
